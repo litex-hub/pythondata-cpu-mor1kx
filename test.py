db22f02a84de6b88b351ae66813f2e7ed2a18f57
LiteX <robot@timvideos.us>
2020-04-06 15:27:52 +0000

    Updating module to 5.0.post119
    Updated data to v5.0-76-g06e2e46 based on 06e2e46afbad62572c31bde88e8f2424e23ecda2 from https://github.com/openrisc/mor1kx.git.
    > commit 06e2e46afbad62572c31bde88e8f2424e23ecda2
    > Author: JaewonHur <57657645+JaewonHur@users.noreply.github.com>
    > Date:   Mon Dec 23 01:23:20 2019 +0900
    >
    >     Fix mor1kx_decode to accept valid l.fl1 (l.ff1) instructions (#110)
    >
    >     * Fix mor1kx_decode to only accept valid l.fl1 and l.ff1 instructions
    >
    >     Both l.fl1 and l.ff1 have bit 9 set in the insruction as port of their op codes,
    >     mor1kx_decode does not currently properly check this
    >
    
    Updated using 0.0.post43 from https://github.com/litex-hub/litex-data-auto

--- test.py
+++ test.py
@@ -3,8 +3,18 @@ import os
 
 from litex.data.cpu import mor1kx
 
-print("Found mor1kx @ version", mor1kx.version_str, "("+mor1kx.git_hash+")")
+print("Found mor1kx @ version", mor1kx.version_str, "(with data", mor1kx.data_version_str, ")")
+print()
 print("Data is in", mor1kx.data_location)
 assert os.path.exists(mor1kx.data_location)
-print("It contains:\n -", end=" ")
-print("\n - ".join(os.listdir(mor1kx.data_location)))
+print("Data is version", mor1kx.data_version_str, mor1kx.data_git_hash)
+print("-"*75)
+print(mor1kx.data_git_msg)
+print("-"*75)
+print()
+print("It contains:")
+for root, dirs, files in os.walk(mor1kx.data_location):
+    dirs.sort()
+    for f in sorted(files):
+        path = os.path.relpath(os.path.join(root, f), mor1kx.data_location)
+        print(" -", path)
