a6e6b3b889d64832e357fe1b82b371be7e267e88
LiteX <robot@timvideos.us>
2020-05-01 17:18:33 +0000

    Updating pythondata-cpu-mor1kx to 5.0.post127
    Updated data to v5.0-76-g06e2e46 based on 06e2e46afbad62572c31bde88e8f2424e23ecda2 from https://github.com/openrisc/mor1kx.git.
    > commit 06e2e46afbad62572c31bde88e8f2424e23ecda2
    > Author: JaewonHur <57657645+JaewonHur@users.noreply.github.com>
    > Date:   Mon Dec 23 01:23:20 2019 +0900
    >
    >     Fix mor1kx_decode to accept valid l.fl1 (l.ff1) instructions (#110)
    >
    >     * Fix mor1kx_decode to only accept valid l.fl1 and l.ff1 instructions
    >
    >     Both l.fl1 and l.ff1 have bit 9 set in the insruction as port of their op codes,
    >     mor1kx_decode does not currently properly check this
    >
    
    Updated using 0.0.post51 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_mor1kx/__init__.py
+++ pythondata_cpu_mor1kx/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/openrisc/mor1kx.git"
 
 # Module version
-version_str = "5.0.post126"
-version_tuple = (5, 0, 126)
+version_str = "5.0.post127"
+version_tuple = (5, 0, 127)
 try:
     from packaging.version import Version as V
-    pversion = V("5.0.post126")
+    pversion = V("5.0.post127")
 except ImportError:
     pass
 
@@ -37,11 +37,11 @@ Date:   Mon Dec 23 01:23:20 2019 +0900
 """
 
 # Tool version info
-tool_version_str = "0.0.post50"
-tool_version_tuple = (0, 0, 50)
+tool_version_str = "0.0.post51"
+tool_version_tuple = (0, 0, 51)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post50")
+    ptool_version = V("0.0.post51")
 except ImportError:
     pass
 
