41d1364a39d04cfe9c535adfca7ff45ce221bb69
LiteX Robot <robot@timvideos.us>
2021-02-04 02:02:36 +0000

    Updating pythondata-cpu-mor1kx to 5.0.post152
    Updated data to v5.0-80-ga9ac821 based on a9ac8213d9f752485a6f2bd46885ca37a53899c2 from https://github.com/openrisc/mor1kx.git.
    > commit a9ac8213d9f752485a6f2bd46885ca37a53899c2
    > Merge: dd01358 cf2bce7
    > Author: Stafford Horne <shorne@gmail.com>
    > Date:   Fri Jan 1 10:49:08 2021 +0900
    >
    >     Merge pull request #116 from openrisc/fix-ci
    >
    >     travis: Env vars are no longer passed to docker, maybe quotes?
    >
    
    Updated using 0.0.post72 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_mor1kx/__init__.py
+++ pythondata_cpu_mor1kx/__init__.py
@@ -4,39 +4,42 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/openrisc/mor1kx.git"
 
 # Module version
-version_str = "5.0.post143"
-version_tuple = (5, 0, 143)
+version_str = "5.0.post152"
+version_tuple = (5, 0, 152)
 try:
     from packaging.version import Version as V
-    pversion = V("5.0.post143")
+    pversion = V("5.0.post152")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "5.0.post77"
-data_version_tuple = (5, 0, 77)
+data_version_str = "5.0.post80"
+data_version_tuple = (5, 0, 80)
 try:
     from packaging.version import Version as V
-    pdata_version = V("5.0.post77")
+    pdata_version = V("5.0.post80")
 except ImportError:
     pass
-data_git_hash = "9679d18eabaf6a15ee56eafdd35812043d9ae193"
-data_git_describe = "v5.0-77-g9679d18"
+data_git_hash = "a9ac8213d9f752485a6f2bd46885ca37a53899c2"
+data_git_describe = "v5.0-80-ga9ac821"
 data_git_msg = """\
-commit 9679d18eabaf6a15ee56eafdd35812043d9ae193
-Author: Andrey Bacherov <bandvig@mail.ru>
-Date:   Sun Jul 26 15:40:12 2020 +0300
+commit a9ac8213d9f752485a6f2bd46885ca37a53899c2
+Merge: dd01358 cf2bce7
+Author: Stafford Horne <shorne@gmail.com>
+Date:   Fri Jan 1 10:49:08 2021 +0900
 
-    Open OPTION_FTOI_ROUNDING as "CPP" in according with spec 1.3.
+    Merge pull request #116 from openrisc/fix-ci
+    
+    travis: Env vars are no longer passed to docker, maybe quotes?
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post66"
-tool_version_tuple = (0, 0, 66)
+tool_version_str = "0.0.post72"
+tool_version_tuple = (0, 0, 72)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post66")
+    ptool_version = V("0.0.post72")
 except ImportError:
     pass
 
