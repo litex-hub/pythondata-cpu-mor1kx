4b99535c58aa3d66b82c078af126ad1dc38af94b
LiteX Robot <robot@timvideos.us>
2021-02-15 20:47:59 +0000

    Updating pythondata-cpu-mor1kx to 5.0.post171
    Updated data to v5.0-80-ga9ac821 based on a9ac8213d9f752485a6f2bd46885ca37a53899c2 from https://github.com/openrisc/mor1kx.git.
    > commit a9ac8213d9f752485a6f2bd46885ca37a53899c2
    > Merge: dd01358 cf2bce7
    > Author: Stafford Horne <shorne@gmail.com>
    > Date:   Fri Jan 1 10:49:08 2021 +0900
    >
    >     Merge pull request #116 from openrisc/fix-ci
    >
    >     travis: Env vars are no longer passed to docker, maybe quotes?
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_mor1kx/__init__.py
+++ pythondata_cpu_mor1kx/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/openrisc/mor1kx.git"
 
 # Module version
-version_str = "5.0.post170"
-version_tuple = (5, 0, 170)
+version_str = "5.0.post171"
+version_tuple = (5, 0, 171)
 try:
     from packaging.version import Version as V
-    pversion = V("5.0.post170")
+    pversion = V("5.0.post171")
 except ImportError:
     pass
 
@@ -35,11 +35,11 @@ Date:   Fri Jan 1 10:49:08 2021 +0900
 """
 
 # Tool version info
-tool_version_str = "0.0.post90"
-tool_version_tuple = (0, 0, 90)
+tool_version_str = "0.0.post91"
+tool_version_tuple = (0, 0, 91)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post90")
+    ptool_version = V("0.0.post91")
 except ImportError:
     pass
 
