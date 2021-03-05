a64170466cdf06702e08287b418a8c102b733a7c
LiteX Robot <robot@timvideos.us>
2021-03-05 20:42:03 +0000

    Updating pythondata-cpu-mor1kx to 5.0.post181
    Updated data to v5.0-87-g0bca071 based on 0bca071fce56caa459d61f6c9cf7e096dc1c5d58 from https://github.com/openrisc/mor1kx.git.
    > commit 0bca071fce56caa459d61f6c9cf7e096dc1c5d58
    > Merge: a9ac821 7eb70ca
    > Author: Stafford Horne <shorne@gmail.com>
    > Date:   Mon Mar 1 06:33:11 2021 +0900
    >
    >     Merge pull request #118 from stffrdhrn/github-ci
    >
    >     Convert Travis CI to GitHub Actions
    >
    
    Updated using 0.0.post94 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_mor1kx/__init__.py
+++ pythondata_cpu_mor1kx/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/openrisc/mor1kx.git"
 
 # Module version
-version_str = "5.0.post179"
-version_tuple = (5, 0, 179)
+version_str = "5.0.post181"
+version_tuple = (5, 0, 181)
 try:
     from packaging.version import Version as V
-    pversion = V("5.0.post179")
+    pversion = V("5.0.post181")
 except ImportError:
     pass
 
@@ -35,11 +35,11 @@ Date:   Mon Mar 1 06:33:11 2021 +0900
 """
 
 # Tool version info
-tool_version_str = "0.0.post92"
-tool_version_tuple = (0, 0, 92)
+tool_version_str = "0.0.post94"
+tool_version_tuple = (0, 0, 94)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post92")
+    ptool_version = V("0.0.post94")
 except ImportError:
     pass
 
