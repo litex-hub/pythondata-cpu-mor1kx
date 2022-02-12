6a46375b949dd0b1233ce205797951e7de251210
LiteX Robot <robot@timvideos.us>
2022-02-12 19:07:02 +0000

    Updating pythondata-cpu-mor1kx to 5.1.post128
    Updated data to v5.1-2-gaca2481 based on aca24812520db8e3a0347ac45b0ac18027ccf053 from https://github.com/openrisc/mor1kx.git.
    > commit aca24812520db8e3a0347ac45b0ac18027ccf053
    > Merge: 16ad5ad 698681d
    > Author: Stafford Horne <shorne@gmail.com>
    > Date:   Wed Jan 12 06:08:22 2022 +0900
    >
    >     Merge pull request #144 from stffrdhrn/synth-params
    >
    >     core: Add parameters to the synth job
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_mor1kx/__init__.py
+++ pythondata_cpu_mor1kx/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/openrisc/mor1kx.git"
 
 # Module version
-version_str = "5.1.post126"
-version_tuple = (5, 1, 126)
+version_str = "5.1.post128"
+version_tuple = (5, 1, 128)
 try:
     from packaging.version import Version as V
-    pversion = V("5.1.post126")
+    pversion = V("5.1.post128")
 except ImportError:
     pass
 
@@ -35,11 +35,11 @@ Date:   Wed Jan 12 06:08:22 2022 +0900
 """
 
 # Tool version info
-tool_version_str = "0.0.post124"
-tool_version_tuple = (0, 0, 124)
+tool_version_str = "0.0.post126"
+tool_version_tuple = (0, 0, 126)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post124")
+    ptool_version = V("0.0.post126")
 except ImportError:
     pass
 
